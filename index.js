03141ce9a7021928d0dd75c3a801a2abba890bae
Flurin Brühwiler <bruhwiler.flurin@gmail.com>
2024-09-28 17:12:34 +0200

    Trying to get the camera working

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+class Vector3 {
+    constructor(x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+
+class Vector4 {
+    constructor(x, y, z, w) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.w = w;
+    }
+}
+
+
+
 const gameInfo = {
     vertices : [],
-    bindGroup: undefined,
     renderPipeline: undefined,
     device: undefined,
     context: undefined
@@ -16,6 +34,10 @@ async function Init() {
     await InitRenderer(canvas)
 
     let frameCount = 0;
+    let camera = {
+        position: new Vector3(10, 0, 10),
+        rotation: new Vector3(0, 0, 1)
+    }
 
     requestAnimationFrame(Frame)
 
@@ -24,17 +46,14 @@ async function Init() {
 
         StartFrame();
 
-        let pos = frameCount % 200;
-        pos = pos / 200;
-
         DrawRectangle(
-            {x: 0, y: 0, z: 0},
-            {x: 0, y: pos -  0.5, z: 0},
-            {x: pos - 0.5, y: pos -  0.5, z: 0},
-            {x: pos -  0.5, y: 0, z: 0},
+            {x: 0, y: 0, z: 10},
+            {x: 0, y: 10, z: 10},
+            {x: 10, y: 10, z: 10},
+            {x: 10, y: 0, z: 10},
         );
 
-        EndFrame();
+        EndFrame(camera);
 
         requestAnimationFrame(Frame);
     }
@@ -42,6 +61,7 @@ async function Init() {
 
 //model: position, rotation, scale of an object
 //view: inverse of the cameras own model matrix
+//projection: to clip space
 
 function StartFrame() {
     gameInfo.vertices = [];
@@ -64,20 +84,6 @@ async function InitRenderer(canvas) {
         format: navigator.gpu.getPreferredCanvasFormat(),
         alphaMode: "premultiplied"
     })
-
-    const mvpData = new Float32Array([
-        1, 0, 0, 0,
-        0, 1, 0, 0,
-        0, 0, 0, 0,
-        0, 0, 0, 1
-    ])
-
-    const uniformBuffer = gameInfo.device.createBuffer({
-        size: 16*4, //4x4 matrix
-        usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST
-    });
-
-    gameInfo.device.queue.writeBuffer(uniformBuffer, 0, mvpData, 0, mvpData.length);
 
     const vertexBuffers = [
         {
@@ -120,13 +126,6 @@ async function InitRenderer(canvas) {
     }
 
     gameInfo.renderPipeline = gameInfo.device.createRenderPipeline(pipelineDescriptor);
-
-    gameInfo.bindGroup = gameInfo.device.createBindGroup({
-        layout: gameInfo.renderPipeline.getBindGroupLayout(0),
-        entries: [
-            { binding: 0, resource: { buffer: uniformBuffer } }
-        ]
-    })
 }
 
 function DrawRectangle(v1, v2, v3, v4) {
@@ -138,7 +137,7 @@ function DrawTriangle(v1, v2, v3) {
     gameInfo.vertices.push(v1, v2, v3)
 }
 
-function EndFrame() {
+function EndFrame(camera) {
 
     const commandEncoder = gameInfo.device.createCommandEncoder();
     const clearColor = { r: 0.0, g: 0.5, b: 1.0, a: 1.0 };
@@ -154,6 +153,7 @@ function EndFrame() {
         ]
     }
 
+    //vertices
     const vertices = new Float32Array(gameInfo.vertices.flatMap(v =>
                                                     [
                                                         v.x, v.y, v.z, 1,
@@ -167,11 +167,33 @@ function EndFrame() {
 
     gameInfo.device.queue.writeBuffer(vertexBuffer, 0, vertices, 0, vertices.length);
 
+    const projectionMatrix = CreateCameraProjectionMatrix(90, 1, 0.1, 1000);
+    const viewMatrix = Matrix4x4.Translation(camera.position);
+
+    const mvpMatrix = projectionMatrix.multiply(viewMatrix);
+
+    //model - view - projection
+    const mvpData = mvpMatrix.toFloat32Array();
+
+    const uniformBuffer = gameInfo.device.createBuffer({
+        size: 16*4, //4x4 matrix
+        usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST
+    });
+
+    gameInfo.device.queue.writeBuffer(uniformBuffer, 0, mvpData, 0, mvpData.length);
+
+    const bindGroup = gameInfo.device.createBindGroup({
+        layout: gameInfo.renderPipeline.getBindGroupLayout(0),
+        entries: [
+            { binding: 0, resource: { buffer: uniformBuffer } }
+        ]
+    })
+
     const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
 
     passEncoder.setPipeline(gameInfo.renderPipeline);
     passEncoder.setVertexBuffer(0, vertexBuffer)
-    passEncoder.setBindGroup(0, gameInfo.bindGroup);
+    passEncoder.setBindGroup(0, bindGroup);
     passEncoder.draw(gameInfo.vertices.length) //vertex count
 
     passEncoder.end();
@@ -179,4 +201,138 @@ function EndFrame() {
     gameInfo.device.queue.submit([commandBuffer]);
 
     vertexBuffer.destroy(); //do I need this?
+}
+
+class Matrix4x4{
+    constructor(m0, m4, m8, m12,
+                m1, m5, m9, m13,
+                m2, m6, m10, m14,
+                m3, m7, m11, m15) {
+        this.m0 = m0;
+        this.m1 = m1;
+        this.m2 = m2;
+        this.m3 = m3;
+        this.m4 = m4;
+        this.m5 = m5;
+        this.m6 = m6;
+        this.m7 = m7;
+        this.m8 = m8;
+        this.m9 = m9;
+        this.m10 = m10;
+        this.m11 = m11;
+        this.m12 = m12;
+        this.m13 = m13;
+        this.m14 = m14;
+        this.m15 = m15;
+    }
+
+    static Identity(){
+        return new Matrix4x4(1,0, 0, 0,
+                            0,  1, 0, 0,
+                            0,  0, 1, 0,
+                            0,  0, 0, 1)
+    }
+
+    static Translation(position){
+        const matrix = this.Identity();
+        matrix.setW(new Vector4(position.x, position.y, position.z, 1));
+        return matrix;
+    }
+
+    multiply(other){
+        return new Matrix4x4(
+DotProduct(this.row1(), other.column1()), DotProduct(this.row1(), other.column2()), DotProduct(this.row1(), other.column3()), DotProduct(this.row1(), other.column4()),
+DotProduct(this.row2(), other.column1()), DotProduct(this.row2(), other.column2()), DotProduct(this.row2(), other.column3()), DotProduct(this.row2(), other.column4()),
+DotProduct(this.row3(), other.column1()), DotProduct(this.row3(), other.column2()), DotProduct(this.row3(), other.column3()), DotProduct(this.row3(), other.column4()),
+DotProduct(this.row4(), other.column1()), DotProduct(this.row4(), other.column2()), DotProduct(this.row4(), other.column3()), DotProduct(this.row4(), other.column4()));
+    }
+
+    row1(){
+        return new Vector4(this.m0, this.m4, this.m8, this.m12)
+    }
+
+    row2(){
+        return new Vector4(this.m1, this.m5, this.m9, this.m13)
+    }
+
+    row3(){
+        return new Vector4(this.m2, this.m6, this.m10, this.m14)
+    }
+
+    row4(){
+        return new Vector4(this.m3, this.m7, this.m11, this.m15)
+    }
+
+    column1(){
+        return new Vector4(this.m0, this.m1, this.m2, this.m3)
+    }
+
+    column2(){
+        return new Vector4(this.m4, this.m5, this.m6, this.m7)
+    }
+
+    column3(){
+        return new Vector4(this.m8, this.m9, this.m10, this.m11)
+    }
+
+    column4(){
+        return new Vector4(this.m12, this.m13, this.m14, this.m15)
+    }
+
+    setX(v){
+        this.m0 = v.x;
+        this.m1 = v.y;
+        this.m2 = v.z;
+        this.m3 = v.w;
+    }
+
+    setY(v){
+        this.m4 = v.x;
+        this.m5 = v.y;
+        this.m6 = v.z;
+        this.m7 = v.w;
+    }
+
+    setZ(v){
+        this.m8 = v.x;
+        this.m9 = v.y;
+        this.m10 = v.z;
+        this.m11 = v.w;
+    }
+
+    setW(v){
+        this.m12 = v.x;
+        this.m13 = v.y;
+        this.m14 = v.z;
+        this.m15 = v.w;
+    }
+
+    toFloat32Array(){
+        return new Float32Array([
+            this.m0, this.m1, this.m2, this.m3,
+            this.m4, this.m5, this.m6, this.m7,
+            this.m8, this.m9, this.m10, this.m11,
+            this.m12, this.m13, this.m14, this.m15,
+        ])
+    }
+}
+
+function CreateCameraProjectionMatrix(fov, aspectRatio, nearDistance, farDistance) {
+    const height = 1 / Math.tan(fov * 0.5);
+    const width = height / aspectRatio;
+    const range = farDistance / (nearDistance - farDistance);
+
+    const matrix = new Matrix4x4();
+    matrix.setX(new Vector4(width, 0, 0, 0));
+    matrix.setY(new Vector4(0, height, 0, 0));
+    matrix.setZ(new Vector4(0, 0, range, 0));
+    matrix.setW(new Vector4(0, 0, 0, range * nearDistance));
+    return matrix;
+}
+
+function DotProduct(v1, v2) {
+    return v1.x * v2.x
+        + v1.y * v2.y
+        + v1.z * v2.z
+        + v1.w * v2.w;
 }
